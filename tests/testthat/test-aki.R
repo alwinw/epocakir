dbf789ee64906d7b287eed936d40d901b759d80a
lymt <66116956+lymt@users.noreply.github.com>
2020-10-12 10:46:04 +1100

    Added unit tests for aki.numeric

--- tests/testthat/test-aki.R
+++ tests/testthat/test-aki.R
@@ -1,5 +1,29 @@
-test_that("aki() idenfies AKI stages", {
-  a <- 1
-  b <- 2
-  expect_equal(aki(a, 0, b), 3)
+test_that("aki() for numeric vector of SCr with no baseline", {
+  SCr <- seq(60, 200, by = 20)
+  aki_stages <- forcats::fct_c(
+    .aki_stages[length(.aki_stages)],
+    .aki_stages[length(.aki_stages)],
+    .aki_stages[1],
+    .aki_stages[2],
+    .aki_stages[2],
+    .aki_stages[2],
+    .aki_stages[3],
+    .aki_stages[3]
+  )
+  expect_equal(aki(SCr), aki_stages)
+})
+
+test_that("aki() for numeric vector of SCr with baseline", {
+  SCr <- seq(60, 200, by = 20)
+  aki_stages <- forcats::fct_c(
+    .aki_stages[length(.aki_stages)],
+    .aki_stages[1],
+    .aki_stages[2],
+    .aki_stages[2],
+    .aki_stages[2],
+    .aki_stages[3],
+    .aki_stages[3],
+    .aki_stages[3]
+  )
+  expect_equal(aki(SCr, bCr = 50), aki_stages)
 })
