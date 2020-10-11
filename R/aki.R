0a1541456157920d53c6e865171614c5f9fe3e3d
lymt <66116956+lymt@users.noreply.github.com>
2020-10-12 09:54:29 +1100

    Styled using styler

--- R/aki.R
+++ R/aki.R
@@ -12,7 +12,7 @@ aki <- function(...) {
   UseMethod("aki")
 }
 
-.aki_stages = factor(c("AKI Stage 1", "AKI Stage 2", "AKI Stage 3", "No AKI"))
+.aki_stages <- factor(c("AKI Stage 1", "AKI Stage 2", "AKI Stage 3", "No AKI"))
 
 #' @rdname aki
 #' @export
@@ -21,7 +21,7 @@ aki.numeric <- function(SCr, UO, bCr = NULL, na.rm = FALSE) {
     stop("One of SCr or UO must be provided")
   }
 
-  if (is.null(bCr)) bCr = min(SCr, na.rm = na.rm)
+  if (is.null(bCr)) bCr <- min(SCr, na.rm = na.rm)
 
   dplyr::case_when(
     SCr > 3.0 * bCr ~ .aki_stages[3],
@@ -35,6 +35,3 @@ aki.numeric <- function(SCr, UO, bCr = NULL, na.rm = FALSE) {
 aki.default <- function(x) {
   factor(x, levels = aki_stages)
 }
-
-
-
--- tests/testthat/test-aki.R
+++ tests/testthat/test-aki.R
@@ -1,5 +1,5 @@
 test_that("aki() idenfies AKI stages", {
-  a = 1
-  b = 2
+  a <- 1
+  b <- 2
   expect_equal(aki(a, 0, b), 3)
 })
