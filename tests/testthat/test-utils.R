18aed69968553a71fa3ee1a4335448f61d548a53
lymt <66116956+lymt@users.noreply.github.com>
2020-10-23 10:52:07 +1100

    Added `fun` to dob2age

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -12,8 +12,68 @@ test_that(".sCr2metric() conversion 353.6 umol/l to 4mg/dl is correct", {
   )
 })
 
-Atest_that(".dob2age() between two dates is valid", {
+test_that("dob2age() between two dates is valid", {
   expect_equal(
-    .dob2age(lubridate::as_date("1990-01-01"))
+    dob2age(
+      dob = lubridate::as_date("1990-01-01"),
+      age_on = lubridate::as_date("2002-01-01")
+    ),
+    lubridate::duration(years = 12)
+  )
+})
+
+test_that("dob2age() for a vector is valid", {
+  expect_equal(
+    dob2age(
+      dob = c(
+        lubridate::as_date("1990-01-01"),
+        lubridate::as_date("1994-01-01"),
+        lubridate::as_date("1998-01-01")
+      ),
+      age_on = lubridate::as_date("2002-01-01")
+    ),
+    c(
+      lubridate::duration(years = 12),
+      lubridate::duration(years = 8),
+      lubridate::duration(years = 4)
+    )
+  )
+})
+
+test_that("dob2age() with `floor` is valid", {
+  expect_equal(
+    dob2age(
+      dob = c(
+        lubridate::as_date("1990-01-01"),
+        lubridate::as_date("1994-01-01"),
+        lubridate::as_date("1998-01-01")
+      ),
+      age_on = lubridate::as_date("2002-12-31"),
+      fun = floor
+    ),
+    c(
+      lubridate::duration(years = 12),
+      lubridate::duration(years = 8),
+      lubridate::duration(years = 4)
+    )
+  )
+})
+
+test_that("dob2age() with `ceiling` is valid", {
+  expect_equal(
+    dob2age(
+      dob = c(
+        lubridate::as_date("1990-01-01"),
+        lubridate::as_date("1994-01-01"),
+        lubridate::as_date("1998-01-01")
+      ),
+      age_on = lubridate::as_date("2001-12-31"),
+      fun = ceiling
+    ),
+    c(
+      lubridate::duration(years = 12),
+      lubridate::duration(years = 8),
+      lubridate::duration(years = 4)
+    )
   )
 })
