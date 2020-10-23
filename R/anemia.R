f658cd58e96f344545a99dfe8700cc00ba355111
lymt <66116956+lymt@users.noreply.github.com>
2020-10-23 17:38:37 +1100

    Thoughts on trying to wrap UseMethod

--- R/anemia.R
+++ R/anemia.R
@@ -3,6 +3,7 @@
 #'
 #' TODO Make consistent with other files
 #'
+#' @param data A data.frame X
 #' @param age The variable name, e.g. "age" or X
 #' @param birthday The variable name, e.g.  "birthday" or X
 #' @param gender The variable name, e.g. "gender" or X
@@ -16,18 +17,41 @@
 #' anemia(1:10)
 anemia <- function(...) {
   ellipsis::check_dots_used()
-  UseMethod("anemia")
+  elli <- list(...)
+  print(names(elli))
+  if ("data" %in% names(elli)) {
+    UseMethod("anemia", elli$data)
+  } else if ("Hb" %in% names(elli)) {
+    UseMethod("anemia", elli$Hb)
+  } else {
+    UseMethod("anemia")
+  }
 }
 
 
 #' @rdname anemia
 #' @export
-anemia.numeric <- function(age, birthday, gender, Hb, ...) {
-
+anemia.numeric <- function(Hb,
+                           age = NULL, birthday = NULL,
+                           gender, ...) {
+  print("numeric!")
+  print(Hb)
 }
 
 #' @rdname anemia
 #' @export
-anemia.units <- function(age, birthday, gender, Hb, ...) {
+anemia.units <- function(Hb,
+                         age = NULL, birthday = NULL,
+                         gender, ...) {
+  print("units!")
+  print(Hb)
+}
 
+#' @rdname anemia
+#' @export
+anemia.default <- function(data = NULL, age = NULL, birthday = NULL,
+                           gender, Hb, ...) {
+  print("default!")
+  print(data)
+  # print(Hb)
 }
