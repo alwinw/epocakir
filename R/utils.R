4b506573e5a56ee92f57bfab4b23af0551d70e7c
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-03-07 10:40:04 +1100

    Tidied up and added tests for as_metric

--- R/utils.R
+++ R/utils.R
@@ -1,17 +1,49 @@
+#' Conversion Factors
+#'
+#' List of conversion factors based on tables in  KDIGO Clinical Practice
+#' Guidelines.
+#'
+#' \describe{
+#' \item{parameter}{Name of the measurement}
+#' \item{metric_units}{Metric units for the parameter}
+#' \item{mol_weight}{Molecular weight (where required)}
+#' \item{description}{Full name}
+#' }
+#' @examples
+#' conversion_factors
+conversion_factors <- tibble::tribble(
+  ~parameter, ~metric_units, ~mol_weight, ~description,
+  # 2012 AKI Guideline
+  "SAmk", "ug/ml", 585.6, "Amikacin (serum, plasma)",
+  "BUN", "mg/dl", 28.014, "Blood urea nitrogen",
+  "SiCa", "mg/dl", 40.08, "Calcium, ionized (serum)",
+  "SCr", "mg/dl", 113.120, "Creatinine (serum)",
+  "CLcr", "ml/min", NA, "Creatinine clearance",
+  "CGen", "ug/ml", 477.596, "Gentamicin (serum)",
+  "Glc", "mg/dl", 180.156, "Glucose",
+  "Lac", "mg/dl", 90.08, "Lactate (plasma)",
+  "STob", "ug/ml", 467.5, "Tobramycin (serum, plasma)",
+  "Urea", "mg/dl", 60.06, "Urea (plasma)" # changed from AKI 2012 Guideline
+) %>%
+  dplyr::mutate(mol_weight = units::set_units(mol_weight, "g/mol"))
+
 #' Convert a measured value to metric units
 #'
+#' @param param (char) Name of measurement, e.g. param = "SCr"
+#' @param meas (units) Measurement or vector of measurements
 #' @param ... (units) One of conversion_factors$parameter,
 #'   e.g. SCr = units::set_units(88.4, "umol/l").
 #'   Case insensitive.
-#' @param param (char) Name of measurement, e.g. param = "SCr"
-#' @param meas (units) Measurement
 #'
-#' @return (units) Converted measured value
+#' @return (units) Converted measured value or vector of measured values
 #' @export
 #'
 #' @examples
-#' as_metric(SCr = units::set_units(88.4, "umol/l"))
 #' as_metric(param = "scr", meas = units::set_units(88.4, "umol/l"))
+#' as_metric("scr", units::set_units(88.4, "umol/l"))
+#'
+#' values <- units::set_units(c(60, 70, 80), "umol/l")
+#' as_metric(SCr = values)
 as_metric <- function(param = NULL, meas = NULL, ...) {
   ellipsis::check_dots_used()
   if (is.null(param) | is.null(meas)) {
@@ -35,23 +67,6 @@ as_metric <- function(param = NULL, meas = NULL, ...) {
     units::set_units(meas, conversion$metric_units, mode = "standard")
   }
 }
-
-conversion_factors <- tibble::tribble(
-  ~parameter, ~metric_units, ~mol_weight, ~description,
-  # 2012 AKI Guideline
-  "SAmk", "ug/ml", 585.6, "Amikacin (serum, plasma)",
-  "BUN", "mg/dl", 28.014, "Blood urea nitrogen",
-  "SiCa", "mg/dl", 40.08, "Calcium, ionized (serum)",
-  "SCr", "mg/dl", 113.120, "Creatinine (serum)",
-  "CLcr", "ml/min", NA, "Creatinine clearance",
-  "CGen", "ug/ml", 477.596, "Gentamicin (serum)",
-  "Glc", "mg/dl", 180.156, "Glucose",
-  "Lac", "mg/dl", 90.08, "Lactate (plasma)",
-  "STob", "ug/ml", 467.5, "Tobramycin (serum, plasma)",
-  "Urea", "mg/dl", 60.06, "Urea (plasma)" # changed from AKI 2012 Guideline
-) %>%
-  dplyr::mutate(mol_weight = units::set_units(mol_weight, "g/mol"))
-
 
 
 #' Calculate age from DOB
@@ -92,6 +107,7 @@ dob2age <- function(dob, age_on = lubridate::today(),
 
 gender2factor <- function() {}
 
+binary2factor <- function() {}
 
 #' Pipe operator
 #'
