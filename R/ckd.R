79f38fa5994d40de594a3eeba0cceb80e9f79512
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-03-07 23:09:22 +1100

    Exported eGFR functions

--- R/ckd.R
+++ R/ckd.R
@@ -1,4 +1,48 @@
-GFR.adult.SCr <- function(SCr, Age, male, black) {
+#' GFR Estimation
+#'
+#' - 2009 CKD-EPI creatinine equation
+#' - 2012 CKD-EPI cystatin C equation
+#' - 2012 CKD-EPI creatinine-cystatin C equation
+#' - Pediatric equations
+#'
+#' @param SCr (units) Serum creatinine, converted to mg/dl
+#' @param SCysC (units) Serum cystatin C, converted to mg/l
+#' @param Age (units) Age of patient, converted to years
+#' @param height (units) Height of patient, converted to m
+#' @param BUN (units) Blood urea nitrogen, converted to mg/dl
+#' @param male (logical) Male or not
+#' @param black (logical) Black race or not
+#' @param pediatric (logical) Paediatric or not
+#'
+#' @return (units) Estimated glomerular filtration rate (eGFR) in ml/min/1.73m2
+#' @export
+#'
+#' @examples
+#' print("todo")
+eGFR <- function(SCr = NULL,
+                 SCysC = NULL,
+                 Age = NULL,
+                 height = NULL,
+                 BUN = NULL,
+                 male = FALSE,
+                 black = FALSE,
+                 pediatric = FALSE) {
+  dplyr::case_when(
+    !pediatric & !is.null(SCr) & is.null(SCysC) ~ eGFR.adult.SCr(SCr, Age, male, black),
+    !pediatric & is.null(SCr) & !is.null(SCysC) ~ eGFR.adult.SCysC(SCysC, Age, male),
+    !pediatric & !is.null(SCr) & !is.null(SCysC) ~ eGFR.adult.SCr_SCysC(SCr, SCysC, Age, male, black),
+    pediatric & !is.null(SCr) & !is.null(height) & is.null(BUN) & is.null(SCysC) ~ eGFR.child.SCr(SCr, height),
+    pediatric & !is.null(SCr) & !is.null(height) & !is.null(BUN) & is.null(SCysC) ~ eGFR.child.SCr_BUN(SCr, height, BUN),
+    pediatric & is.null(SCr) & !is.null(SCysC) ~ GFR.child.SCysC(SCysC),
+    TRUE ~ NA_real_
+  )
+}
+
+
+#' @rdname eGFR
+#' @export
+eGFR.adult.SCr <- function(SCr, Age, male, black) {
+  # convert units here
   kappa <- dplyr::if_else(!male, 0.7, 0.9)
   alpha <- dplyr::if_else(!male, -0.329, -0.411)
   141 * min(SCr / kappa, 1)^alpha * max(SCr / kappa, 1)^-1.209 * 0.993^Age *
@@ -7,35 +51,21 @@ GFR.adult.SCr <- function(SCr, Age, male, black) {
 }
 
 
-GFR <- function(SCr = NULL,
-                SCysC = NULL,
-                Age = NULL,
-                height = NULL,
-                BUN = NULL,
-                male = FALSE,
-                black = FALSE,
-                pediatric = FALSE) {
-  dplyr::case_when(
-    !pediatric & !is.null(SCr) & is.null(SCysC) ~ GFR.adult.SCr(SCr, Age, male, black),
-    TRUE ~ NA_real_
-  )
-}
-
-
-
-# Adult
-GFR.SCr <- function() {
-
-  # Then set units of GFR
-}
-
-
-GFR.SCysC <- function() {
+#' @rdname eGFR
+#' @export
+eGFR.adult.SCysC <- function(SCysC, Age, male) {
+  # convert units here
   133 * min(SCysC / 0.8, 1)^-0.499 * max(SCysC / 0.8, 1)^-1.328 * 0.996^Age *
     dplyr::if_else(male, 1, 0.932)
 }
 
-GFR.SCr.SCysC <- function() {
+
+#' @rdname eGFR
+#' @export
+eGFR.adult.SCr_SCysC <- function(SCr, SCysC, Age, male, black) {
+  # convert units here
+  kappa <- dplyr::if_else(!male, 0.7, 0.9)
+  alpha <- dplyr::if_else(!male, -0.248, -0.207)
   135 * min(SCr / kappa, 1)^alpha * max(SCr / kappa, 1)^-0.601 *
     min(SCysC / 0.8, 1)^-0.375 * max(SCysC / 0.8, 1)^-0.711 *
     0.995^Age *
@@ -43,15 +73,27 @@ GFR.SCr.SCysC <- function() {
     dplyr::if_else(black, 1.08, 1)
 }
 
-# Pediatric
-GFR.SCr <- function() {
-  41.3 * height / SCr |
-    40.7 * (height / SCr)^0.4 * (30 / BUN)^0.202
+
+#' @rdname eGFR
+#' @export
+eGFR.child.SCr <- function(SCr, height) {
+  41.3 * (height / SCr)
 }
 
-GFR.SCysC <- function() {
+
+#' @rdname eGFR
+#' @export
+eGFR.child.SCr_BUN <- function(SCr, height, BUN) {
+  40.7 * (height / SCr)^0.64 * (30 / BUN)^0.202
+}
+
+
+#' @rdname eGFR
+#' @export
+eGFR.child.SCysC <- function(SCysC) {
   70.69 * (SCysC)^-0.931
 }
+
 
 GFR.levels <- function() {
   dplyr::case_when(
