4120148daaffb735646ec33c0d23c5afc49fb3fb
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-03-08 11:22:58 +1100

    Changed conversion_factors to internal data.frame

--- R/utils.R
+++ R/utils.R
@@ -23,8 +23,22 @@ NULL
 #' \item{description}{Full name}
 #' }
 #' @examples
-#' conversion_factors
-"conversion_factors"
+#' epocakir:::conversion_factors
+conversion_factors <- tibble::tribble(
+  ~parameter, ~metric_units, ~mol_weight, ~description,
+  # 2012 AKI Guideline
+  "SAmk", "ug/ml", 585.6, "Amikacin (serum, plasma)",
+  "BUN", "mg/dl", 28.014, "Blood urea nitrogen",
+  "SiCa", "mg/dl", 40.08, "Calcium, ionized (serum)",
+  "SCr", "mg/dl", 113.120, "Creatinine (serum)",
+  "CLcr", "ml/min", NA, "Creatinine clearance",
+  "CGen", "ug/ml", 477.596, "Gentamicin (serum)",
+  "Glc", "mg/dl", 180.156, "Glucose",
+  "Lac", "mg/dl", 90.08, "Lactate (plasma)",
+  "STob", "ug/ml", 467.5, "Tobramycin (serum, plasma)",
+  "Urea", "mg/dl", 60.06, "Urea (plasma)" # changed from AKI 2012 Guideline
+) %>%
+  dplyr::mutate(mol_weight = units::set_units(mol_weight, "g/mol"))
 
 
 #' Convert a measured value to metric units
