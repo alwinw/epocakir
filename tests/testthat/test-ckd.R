3892054b5eec41b7e46d907b8e06076431c8fc51
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-03-08 22:38:14 +1100

    Refactored test-ckd

--- tests/testthat/test-ckd.R
+++ tests/testthat/test-ckd.R
@@ -1,25 +1,24 @@
 test_that("eGFR.adult.SCr()", {
-  df <- tibble::tibble(
-    a1 = eGFR.adult.SCr(
-      SCr = units::set_units(0.5, "mg/dl"),
-      Age = units::set_units(30, "years"),
-      male = FALSE,
-      black = FALSE
-    ),
-    a2 = eGFR.adult.SCr(
-      SCr = units::set_units(0.5, "mg/dl"),
-      Age = units::set_units(30, "years"),
-      male = FALSE,
-      black = TRUE
-    ),
-  ) %>% dplyr::mutate(
-    dplyr::across(dplyr::everything(), round, 0)
-  )
-  ep <- tibble::tibble(
-    a1 = 144 * (0.5 / 0.7)^-0.329 * 0.993^30,
-    a2 = 144 * (0.5 / 0.7)^-0.329 * 0.993^30 * 1.159,
-  ) %>% dplyr::mutate(
-    dplyr::across(dplyr::everything(), round, 0)
-  )
-  expect_equal(df, ep)
+  df <- tibble::tribble(
+    ~SCr, ~Age, ~male, ~black,
+    0.5, 30, FALSE, FALSE,
+    0.5, 40, FALSE, TRUE,
+    1.5, 30, FALSE, FALSE,
+    1.5, 40, FALSE, TRUE
+  ) %>%
+    dplyr::mutate(
+      SCr = units::set_units(SCr, "mg/dl"),
+      Age = units::set_units(Age, "years"),
+      eGFR = eGFR.adult.SCr(SCr, Age, male, black)
+    ) %>%
+    dplyr::pull(eGFR)
+
+  ep <- units::set_units(c(
+    144 * (0.5 / 0.7)^-0.329 * 0.993^30,
+    144 * (0.5 / 0.7)^-0.329 * 0.993^40 * 1.159,
+    141 * (1.5/0.7)^-1.209 * 0.993^30,
+    141 * (1.5/0.7)^-1.209 * 0.993^40 * 1.159
+  ), "mL/min/1.73m2")
+
+  expect_lte(max(abs(df - ep)), units::set_units(1, "mL/min/1.73m2"))
 })
