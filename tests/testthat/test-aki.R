7827f50c1957af623dcb286268c07d34dc8ade5f
AlwinW <16846521+AlwinW@users.noreply.github.com>
2021-03-10 20:57:00 +1100

    Changed to vctrs::vec_c

--- tests/testthat/test-aki.R
+++ tests/testthat/test-aki.R
@@ -2,7 +2,7 @@
 
 test_that("aki() for numeric vector of SCr with no baseline", {
   SCr <- seq(60, 200, by = 20)
-  aki_stages <- forcats::fct_c(
+  aki_stages <- vctrs::vec_c(
     aki_stages[length(aki_stages)],
     aki_stages[length(aki_stages)],
     aki_stages[1],
@@ -17,7 +17,7 @@ test_that("aki() for numeric vector of SCr with no baseline", {
 
 test_that("aki() for numeric vector of SCr with baseline", {
   SCr <- seq(60, 200, by = 20)
-  aki_stages <- forcats::fct_c(
+  aki_stages <- vctrs::vec_c(
     aki_stages[length(aki_stages)],
     aki_stages[1],
     aki_stages[2],
@@ -32,7 +32,7 @@ test_that("aki() for numeric vector of SCr with baseline", {
 
 test_that("aki() for vector of SCr in mg/dl with no baseline", {
   SCr <- units::set_units(seq(1.5, 4.5, by = 0.5), "mg/dl")
-  aki_stages <- forcats::fct_c(
+  aki_stages <- vctrs::vec_c(
     NA,
     NA,
     aki_stages[1],
@@ -47,7 +47,7 @@ test_that("aki() for vector of SCr in mg/dl with no baseline", {
 test_that("aki() for vector of SCr in mg/dl with baseline", {
   SCr <- units::set_units(seq(2.0, 4.5, by = 0.5), "mg/dl")
   bCr <- units::set_units(1.5, "mg/dl")
-  aki_stages <- forcats::fct_c(
+  aki_stages <- vctrs::vec_c(
     aki_stages[length(aki_stages)],
     aki_stages[1],
     aki_stages[2],
