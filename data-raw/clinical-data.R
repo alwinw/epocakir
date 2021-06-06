9a8f14c32e809dbb683534743d49554549e54589
alwinw <16846521+alwinw@users.noreply.github.com>
2021-06-06 14:23:05 +1000

    Exploration of clinical_obvs

--- data-raw/clinical-data.R
+++ data-raw/clinical-data.R
@@ -1,8 +1,10 @@
 clinical_obvs <- tibble::tribble(
-  ~`Patient Number`, ~`Admission Date`, ~`Admission Time`, ~`Discharge_date`, ~`Discharge_time`, ~`Date of Birth`, ~`Male`,
-  "p10001", "05/03/2020", "14:01:00", "10/03/2020", "16:34:00", "09/01/1956", TRUE,
-  "p10002", "06/03/2020", "09:10:00", "16/03/2020", "18:51:00", "04/12/1997", TRUE,
-  "p10003", "17/03/2020", "12:48:00", "18/03/2020", "09:12:00", "28/05/1973", TRUE,
+  ~`Patient Number`, ~`Admission Date`, ~`Admission Time`, ~`Discharge_date`, ~`Discharge_time`,
+  ~`Date of Birth`, ~`Male`, ~`Height`, ~`Surgery`,
+  "p10001", "2020-03-05", "14:01:00", "2020-03-10", "16:34:00", "1956-01-09", TRUE, 182, FALSE,
+  "p10002", "2020-03-06", "09:10:00", "2020-03-16", "18:51:00", "1997-12-04", FALSE, 161, FALSE,
+  "p10003", "2020-03-17", "12:48:00", "2020-03-18", "09:12:00", "1973-05-28", TRUE, 168, TRUE,
 )
 
+# Consider finding an alternative with real, open access data instead
 usethis::use_data(clinical_obvs, overwrite = TRUE)
